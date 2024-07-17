0628a7b4b99165696a7cd6e604077accf6fc457c
Mohammadsarairha <mohammadsarairha@gmail.com>
2024-07-17 12:40:03 +0300

    Add Use Cors

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -25,6 +25,7 @@ builder.AddInjection();
 builder.AddAuthenticationToken();
 
 var app = builder.Build();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200/"));
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
